8ed4ae88d4ba0688bf592acf607a1c1bf5691e56
Ivan <vvaannaa1@rambler.ru>
2021-04-09 11:26:39 +0200

    Create useMembers.ts

---
+++ useMembers.ts
@@ -0,0 +1,23 @@
+import {useRef} from "react";
+
+export class MembersBase<T> {
+    protected deps: T;
+    setDeps(d: T) {
+        this.deps = d;
+        this.afterSetDeps();
+    }
+    /**
+     * for overloading in inherited classes
+     */
+    protected afterSetDeps() {}
+}
+
+export function useMembers<D, T extends MembersBase<D>>(ctor: (new () => T), deps:  (T extends MembersBase<infer D> ? D : never)): T {
+    const ref = useRef<T>();
+    if (!ref.current) {
+        ref.current = new ctor();
+    }
+    const rv = ref.current;
+    rv.setDeps(deps);
+    return rv;
+}
